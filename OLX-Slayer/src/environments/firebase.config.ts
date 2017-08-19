7a5cf175063c0ddcb1c84084e872ed8262d47aec
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-08-19 13:50:37 +0300

    added firebase config file

---
+++ OLX-Slayer/src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAsM6F9EvnRsA-H87JYaMPTDpQJ8VAg-54",
+    authDomain: "olx-slayer.firebaseapp.com",
+    databaseURL: "https://olx-slayer.firebaseio.com",
+    projectId: "olx-slayer",
+    storageBucket: "",
+    messagingSenderId: "562014221944"
+  };
