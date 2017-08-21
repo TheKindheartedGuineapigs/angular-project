f71be9eea9f09c7feb8b07cf8e09de0f344b9012
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-08-22 00:06:00 +0300

    added providers and router module

--- OLX-Slayer/src/app/app.module.ts
+++ OLX-Slayer/src/app/app.module.ts
@@ -1,6 +1,11 @@
+import { UserService } from './services/user.services';
+import { RoutingModule } from './router/app.router';
+import { RouterModule } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 import { firebaseConfig } from '../environments/firebase.config';
 import { AppComponent } from './app.component';
@@ -11,9 +16,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig),
+    AuthModule,
+    RouterModule,
+    RoutingModule
   ],
-  providers: [],
+  providers: [
+    UserService,
+    AngularFireAuth
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
