3bd5a74c4c18812794889ac1252a19dc28da506a
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-08-23 20:11:16 +0300

    added modal show for register and log in

--- OLX-Slayer/src/app/home/navigation/navigation.component.ts
+++ OLX-Slayer/src/app/home/navigation/navigation.component.ts
@@ -1,15 +1,23 @@
+import { LoginComponent } from './../../auth/components/login/login.component';
+import { SignUpComponent } from './../../auth/components/signup/signup.component';
 import { Component, OnInit } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent {
+  bsModalRef: BsModalRef;
+  constructor(private modalService: BsModalService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  public openSignUpModal() {
+    this.bsModalRef = this.modalService.show(SignUpComponent);
   }
 
+  public openLogInModal() {
+    this.bsModalRef = this.modalService.show(LoginComponent);
+  }
 }
