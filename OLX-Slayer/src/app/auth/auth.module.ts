d1d486eb211f81f015865f2b38515c00e9fddd5e
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-08-23 20:15:36 +0300

    modified

--- OLX-Slayer/src/app/auth/auth.module.ts
+++ OLX-Slayer/src/app/auth/auth.module.ts
@@ -1,8 +1,12 @@
+import { AngularFireAuth } from 'angularfire2/auth';
+import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
+import { UserService } from './../services/user.services';
 import { SignUpComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ModalModule } from 'ngx-bootstrap';
 
 @NgModule({
     declarations: [
@@ -11,7 +15,14 @@ import { CommonModule } from '@angular/common';
     ],
     imports: [
         FormsModule,
-        CommonModule
+        CommonModule,
+        ModalModule.forRoot(),
+    ],
+    entryComponents: [SignUpComponent],
+    providers: [
+        UserService,
+        BsModalRef,
+        AngularFireAuth
     ],
 })
 export class AuthModule {}
