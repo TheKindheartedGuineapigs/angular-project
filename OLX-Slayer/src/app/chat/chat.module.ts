e55975a44f82d9a50bb1228fd01c1506169275cb
vixataaa <vmetodiev93@gmail.com>
2017-08-24 15:06:35 +0300

    Init structure

---
+++ OLX-Slayer/src/app/chat/chat.module.ts
@@ -0,0 +1,19 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChatroomComponent } from './chatroom/chatroom.component';
+import { ChatlistComponent } from './chatlist/chatlist.component';
+
+const appRoutes: Routes = [
+  { path: 'chats', component: ChatlistComponent },
+  { path: 'chats/:username', component: ChatroomComponent },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    CommonModule
+  ],
+  declarations: [ChatroomComponent, ChatlistComponent]
+})
+export class ChatModule { }
