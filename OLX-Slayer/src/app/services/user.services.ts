a24460b2983725ce12ddf753bdd2adac3f314e17
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-09-06 00:32:41 +0300

    added methods for updating and getting profile info

--- OLX-Slayer/src/app/services/user.services.ts
+++ OLX-Slayer/src/app/services/user.services.ts
@@ -1,42 +1,73 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase';
 
 @Injectable()
 export class UserService {
-    private user: Observable<firebase.User>;
-    constructor (private afAuth: AngularFireAuth) {
-        this.user = afAuth.authState;
+    private user: firebase.User;
+    constructor(private afAuth: AngularFireAuth, private db: AngularFireDatabase) {
+        afAuth.authState.subscribe(user => {
+            this.user = user;
+        });
     }
-
-    getCurrentUser (): Observable<firebase.User> {
+    getCurrentUser(): firebase.User {
         return this.user;
     }
-    signup (formData) {
+    signup(formData): firebase.Promise<any> {
         return this.afAuth.auth.createUserWithEmailAndPassword(formData.value.email, formData.value.password);
     }
 
-    login (formData) {
+    login(formData): firebase.Promise<any> {
         return this.afAuth.auth.signInWithEmailAndPassword(formData.value.email, formData.value.password);
     }
 
-    logout () {
+    logout() {
         this.afAuth.auth.signOut();
     }
 
-    updateUserDetails (name: string, photoUrl: string) {
-        this.user.subscribe(user => {
-            const profile = {
-                displayName: name,
-                photoURL: photoUrl
-            };
-            user.updateProfile(profile)
-                .then(() => {
-                    console.log('success, Img uploaded.');
-                }).catch(err => {
-                    console.log(err);
-                });
-        });
+    resetPasswor(email): firebase.Promise<any> {
+        return this.afAuth.auth.sendPasswordResetEmail(email);
+    }
+
+    getUserDetails(uid): FirebaseObjectObservable<any> {
+        return this.db.object('/usersDetails/' + uid + '/details');
+    }
+
+    getUserAddress(uid): FirebaseObjectObservable<any> {
+        return this.db.object('/usersDetails/' + uid + '/address');
+    }
+
+    updateUserProfile(name: string, photoUrl: string) {
+        const profile = {
+            displayName: name,
+            photoURL: photoUrl
+        };
+
+        this.user.updateProfile(profile)
+            .then(() => {
+                console.log('success, Img uploaded.');
+            }).catch(err => {
+                console.log(err);
+            });
+    }
+
+    updateAddress(address, uid) {
+        this.db.object('/usersDetails/' + uid + '/address').set(address)
+            .then(() => {
+                console.log('address updated');
+            }).catch(err => {
+                console.log(err);
+            });
+    }
+
+    updatePersonalDetails(details, uid) {
+        this.db.object('/usersDetails/' + uid + '/details').set(details)
+            .then(() => {
+                console.log('details updated');
+            }).catch(err => {
+                console.log(err);
+            });
     }
 }
