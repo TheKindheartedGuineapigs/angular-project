7e60b4dd4017ff5508ac8d132fec06147d8a8614
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-09-06 02:44:22 +0300

    nav bar changes if you are loged on or not

--- OLX-Slayer/src/app/home/navigation/navigation.component.ts
+++ OLX-Slayer/src/app/home/navigation/navigation.component.ts
@@ -1,10 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
-  constructor() {}
+export class NavigationComponent{
+  private isLogedIn: boolean;
+  constructor(private auth: AngularFireAuth) {
+    this.auth.authState.subscribe(user => {
+      if (user) {
+        this.isLogedIn = true;
+      } else {
+        this.isLogedIn = false;
+      }
+    });
+  }
+  signOut () {
+    this.auth.auth.signOut();
+  }
 }
