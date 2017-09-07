42ac0d1a16ceaf2dadb8c1a178b04653c3504914
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-09-07 22:46:03 +0300

    modified

--- OLX-Slayer/src/app/auth/auth.module.ts
+++ OLX-Slayer/src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
+import { Routes, RouterModule } from '@angular/router';
+import { AuthService } from './../services/auth.service';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 import { UserService } from './../services/user.services';
 import { SignUpComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
@@ -7,6 +8,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AlertModule } from 'ngx-bootstrap/alert';
+
+const appRoutes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignUpComponent }
+  ];
 
 @NgModule({
     declarations: [
@@ -16,15 +22,16 @@ import { AlertModule } from 'ngx-bootstrap/alert';
     imports: [
         FormsModule,
         CommonModule,
-        AlertModule.forRoot()
+        AlertModule.forRoot(),
+        RouterModule.forRoot(appRoutes)
     ],
     entryComponents: [
         SignUpComponent,
         LoginComponent],
     providers: [
         UserService,
-        BsModalRef,
-        AngularFireAuth
+        AngularFireAuth,
+        AuthService
     ],
 })
 export class AuthModule {}
