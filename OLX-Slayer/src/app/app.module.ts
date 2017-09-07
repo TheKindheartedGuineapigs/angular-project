cbec842f88b25e6c2d13311d8dd3ba250b587a3f
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-09-07 22:46:21 +0300

    modified

--- OLX-Slayer/src/app/app.module.ts
+++ OLX-Slayer/src/app/app.module.ts
@@ -1,8 +1,8 @@
+import { UserProfilesModule } from './user.profile/user.profiles.module';
 import { AdvertismentService } from './services/advertisments.service';
 import { CreateAdvertismentComponent } from './components/create.advertisment/create.advertisment.component';
 import { ChatModule } from './chat/chat.module';
 import { ImgurService } from './services/imgur.service';
-import { UserProfileComponent } from './components/user.profile/user.profile.component';
 import { SignUpComponent } from './auth/components/signup/signup.component';
 import { FormsModule } from '@angular/forms';
 import { UserService } from './services/user.services';
@@ -18,14 +18,12 @@ import { AuthGuardService } from './services/auth.guard.service';
 
 import { firebaseConfig } from '../environments/firebase.config';
 import { AppComponent } from './app.component';
-import { TabsModule } from 'ngx-bootstrap';
 import { NavigationComponent } from '../app/home/navigation/navigation.component';
 import { MdToolbarModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SearchComponent } from '../app/home/search/search.component';
 import { DummyComponent } from './dummy/dummy.component';
 import { HttpModule } from '@angular/http';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AlertModule } from 'ngx-bootstrap/alert';
 
 
@@ -35,7 +33,6 @@ import { AlertModule } from 'ngx-bootstrap/alert';
     NavigationComponent,
     SearchComponent,
     DummyComponent,
-    UserProfileComponent,
     CreateAdvertismentComponent
   ],
   imports: [
@@ -49,16 +46,15 @@ import { AlertModule } from 'ngx-bootstrap/alert';
     FormsModule,
     ChatModule,
     HttpModule,
-    TabsModule.forRoot(),
-    BsDropdownModule.forRoot(),
-    AlertModule.forRoot()
+    AlertModule.forRoot(),
+    UserProfilesModule
   ],
   exports: [NavigationComponent],
   providers: [
     ImgurService,
     AngularFireDatabase,
     AuthGuardService,
-    AdvertismentService
+    AdvertismentService,
   ],
   bootstrap: [AppComponent]
 })
