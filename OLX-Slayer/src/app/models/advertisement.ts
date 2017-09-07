78cb4c07b26b5c54b8af4078a14af7fe27e30265
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-09-07 22:43:15 +0300

    added fields

--- OLX-Slayer/src/app/models/advertisement.ts
+++ OLX-Slayer/src/app/models/advertisement.ts
@@ -1,1 +1,24 @@
-export class Advertisement {   }
+export class Advertisement {
+    public heading: string;
+    public category: string;
+    public description: string;
+    public images: [string];
+    public createdBy: string;
+    public city: string;
+    public address: string;
+    public phoneNumber: string;
+    public username: string;
+
+    constructor(heading, category, description, images, createdBy, city, address, phoneNumber, username) {
+        this.address = address;
+        this.category = category;
+        this.city = city;
+        this.createdBy = createdBy;
+        this.description = description;
+        this.heading = heading;
+        this.images = images;
+        this.phoneNumber = phoneNumber;
+        this.username = username;
+    }
+
+}
