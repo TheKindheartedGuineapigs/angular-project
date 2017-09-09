c2133b74480be41b5e445719c108ba76d064b901
vixataaa <vmetodiev93@gmail.com>
2017-09-09 15:46:01 +0300

    Chatroom working with logged user

--- OLX-Slayer/src/app/chat/chatroom/chatroom.component.ts
+++ OLX-Slayer/src/app/chat/chatroom/chatroom.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './../../services/user.services';
 import { Chat } from './../../models/chat';
 import { ChatService } from './../../services/chat.service';
 import { ActivatedRoute } from '@angular/router';
@@ -9,34 +10,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chatroom.component.css']
 })
 export class ChatroomComponent implements OnInit {
-  loggedUsername = 'tosho';
+  userId: string;
+  loggedUsername: string;
   otherUsername: string;
   chat: Chat;
   isDataLoaded: boolean;
 
-  constructor(private route: ActivatedRoute, private chatService: ChatService) {
-    this.isDataLoaded = false;
+  constructor(private route: ActivatedRoute,
+    private chatService: ChatService,
+    private userService: UserService) {
+    const loggedUser = this.route.snapshot.data['user'];
+
+    if (!loggedUser) {
+      // Handle error
+    }
+
+    this.userId = loggedUser.uid;
     this.otherUsername = route.snapshot.params.username;
+    this.isDataLoaded = false;
   }
 
   ngOnInit() {
-    this.loadChat(this.loggedUsername, this.otherUsername);
+    if (this.userId.length === 0) {
+      // Handle error
+    }
+
+    this.userService.getUserDetails(this.userId)
+      .flatMap((res) => {
+        if (!res.username) {
+          // Handle error
+        }
+
+        this.loggedUsername = res.username;
+
+        return this.loadChat(this.loggedUsername, this.otherUsername);
+      })
+      .map((res) => res.json())
+      .subscribe((res) => {
+        if (!res.result) {
+          // Handle error
+        }
+
+        if (Array.isArray(res.result)) {
+          this.chat = res.result[0];
+        } else {
+          this.chat = res.result;
+        }
+
+        this.isDataLoaded = true;
+      });
   }
 
   reloadData() {
-    this.loadChat(this.loggedUsername, this.otherUsername);
+    this.loadChat(this.loggedUsername, this.otherUsername)
+      .map((res) => res.json())
+      .subscribe((res) => {
+        if (!res.result) {
+          // Handle error
+        }
+
+        this.chat = res.result;
+      });
   }
 
-  private  loadChat(user1, user2) {
-    return this.chatService.loadChat(user1, user2)
-    .map(res => res.json())
-    .subscribe((res) => {
-      if (Array.isArray(res.result)) {
-        this.chat = res.result[0];
-      } else {
-        this.chat = res.result;
-      }
-      this.isDataLoaded = true;
-    });
+  private loadUserDetails(uid: string) {
+    return this.userService.getUserDetails(uid);
+  }
+
+  private loadChat(user1: string, user2: string) {
+    return this.chatService.loadChat(user1, user2);
   }
 }
